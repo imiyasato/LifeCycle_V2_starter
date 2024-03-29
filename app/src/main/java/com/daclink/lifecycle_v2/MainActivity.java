df5f70f3531fc40ad70b463c1e9014021e357c6b
imiyasato <imiyasato@csumb.edu>
2024-03-28 18:13:03 -0700

    first commit with autoformat

--- app/src/main/java/com/daclink/lifecycle_v2/MainActivity.java
+++ app/src/main/java/com/daclink/lifecycle_v2/MainActivity.java
@@ -15,86 +15,86 @@ import com.daclink.lifecycle_v2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "LifeCycleDemo";
-    private static final String BUTTON_STATE = "ButtonState";
+  private static final String TAG = "LifeCycleDemo";
+  private static final String BUTTON_STATE = "ButtonState";
 
-    ActivityMainBinding binding;
+  ActivityMainBinding binding;
 
-    Button button;
-    TextView mTextView;
-    boolean messageOne = false;
+  Button button;
+  TextView mTextView;
+  boolean messageOne = false;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        setContentView(binding.getRoot());
+    setContentView(binding.getRoot());
 
-        if (savedInstanceState != null) {
-            messageOne = savedInstanceState.getBoolean(BUTTON_STATE, true);
-        }
+    if (savedInstanceState != null) {
+      messageOne = savedInstanceState.getBoolean(BUTTON_STATE, true);
+    }
 
-        button = binding.button;
-        mTextView = binding.textView;
+    button = binding.button;
+    mTextView = binding.textView;
+    getMessage();
+
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        messageOne = !messageOne;
         getMessage();
+      }
+    });
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                messageOne = !messageOne;
-                getMessage();
-            }
-        });
+    //TODO: add long click listener
 
-        //TODO: add long click listener
+  }
 
+  private void getMessage() {
+    if (messageOne) {
+      mTextView.setText(R.string.message2);
+    } else {
+      mTextView.setText(R.string.message1);
     }
+  }
 
-    private void getMessage(){
-        if (messageOne){
-            mTextView.setText(R.string.message2);
-        } else {
-            mTextView.setText(R.string.message1);
-        }
-    }
+  @Override
+  public void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
+  @Override
+  public void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState called");
+    savedInstanceState.putBoolean(BUTTON_STATE, messageOne);
+  }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "onSaveInstanceState called");
-        savedInstanceState.putBoolean(BUTTON_STATE, messageOne);
-    }
+  @Override
+  public void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
-    }
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
 }
